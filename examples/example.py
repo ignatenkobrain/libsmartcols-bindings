e2de8ebe25e5f2e3a76dcff090a0b702e8fc983d
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2016-02-03 01:24:05 +0100

    example.py: move to __main__
    Signed-off-by: Igor Gnatenko <i.gnatenko.brain@gmail.com>

--- examples/example.py
+++ examples/example.py
@@ -1,38 +1,39 @@
 from __future__ import print_function
 from smartcols import *
 
-tb = Table()
+if __name__ == "__main__":
+    tb = Table()
 
-cl_name = Column("NAME", 0.1, tree=True)
-tb.add_column(cl_name)
+    cl_name = Column("NAME", 0.1, tree=True)
+    tb.add_column(cl_name)
 
-cl_age = tb.new_column("AGE", 2)
-cl_age.right = True
+    cl_age = tb.new_column("AGE", 2)
+    cl_age.right = True
 
-print("Enable colors:", tb.colors)
-tb.colors = True
-print("Enable colors:", tb.colors)
+    print("Enable colors:", tb.colors)
+    tb.colors = True
+    print("Enable colors:", tb.colors)
 
-ln = gdad = tb.new_line()
-ln[cl_name] = "Grandfather Bob"
-ln[cl_age] = "61"
-ln[cl_age].color = "red"
+    ln = gdad = tb.new_line()
+    ln[cl_name] = "Grandfather Bob"
+    ln[cl_age] = "61"
+    ln[cl_age].color = "red"
 
-ln = dad = tb.new_line(ln)
-ln[cl_name] = "Father Adam"
-ln[cl_age] = "38"
+    ln = dad = tb.new_line(ln)
+    ln[cl_name] = "Father Adam"
+    ln[cl_age] = "38"
 
-ln = tb.new_line(dad)
-ln[cl_name] = "Baby Val"
-ln[cl_age] = "9"
+    ln = tb.new_line(dad)
+    ln[cl_name] = "Baby Val"
+    ln[cl_age] = "9"
 
-ln = tb.new_line(dad)
-ln[cl_name] = "Baby Dilbert"
-ln[cl_age] = "5"
+    ln = tb.new_line(dad)
+    ln[cl_name] = "Baby Dilbert"
+    ln[cl_age] = "5"
 
-ln = tb.new_line(gdad)
-ln[0] = "Aunt Gaga"
-ln[1] = "35"
+    ln = tb.new_line(gdad)
+    ln[0] = "Aunt Gaga"
+    ln[1] = "35"
 
-print(tb.json())
-print(tb)
+    print(tb.json())
+    print(tb)
